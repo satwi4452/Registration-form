0c0f7cc82900e899502435e727e1deba643e19e4
satwi4452 <119672157+satwi4452@users.noreply.github.com>
2022-12-15 00:26:10 +0530

    Update ragistration.js

--- ragistration.js
+++ ragistration.js
@@ -1,6 +1,9 @@
 
 function result(){
  
+ 
+function result(){
+ 
  let x=document.getElementById("fname").value;
  console.log(typeof(x));
  console.log(x);
@@ -9,115 +12,80 @@ function result(){
  console.log(y);
  let z=x+y;
  document.getElementById("add").innerHTML=z;
- document.write(z); 
-
- let mail=document.getElementById("inputEmail4").value;
  
- console.log(typeof(mail));
- document.getElementById("email1").innerHTML=mail;
- document.write(mail);
- console.log(mail);
 
- let pn=document.getElementById("inputPassword4").value;
- console.log(pn);
+
+ 
+let r=document.getElementById('Email4').value;
+ document.getElementById('email1').innerHTML=r;
+ 
+ 
+//  let mail=document.getElementById("inputEmail4").value;
+ 
+//  console.log(typeof(mail));
+//  document.getElementById("email1").innerHTML=mail;
+//  document.write(mail);
+//  console.log(mail);
+
+ let pn=document.getElementById("PNO").value;
  document.getElementById("Phoneno").innerHTML=pn;
- document.write(pn);
-
- let ad=document.getElementById("inputAddress").value;
- console.log(ad);
- document.getElementById("address").innerHTML=ad;
- document.write(ad);
-
- let c=document.getElementById("inputCity").value;
- console.log(c);
- document.getElementById("city").innerHTML=c;
- document.write(c);
-
- let s=document.getElementById("State").value;
- console.log(s);
- document.getElementById("state").innerHTML=s;
- document.write(s);
-
- let pin=document.getElementById("Pin").value;
- console.log(pin);
- document.getElementById("pin").innerHTML=pin;
- document.write(pin);
-
- let gm=document.getElementById("flexRadioDefault1").value;
- console.log(gm);
- document.getElementById("gender").innerHTML=gm;
- document.write(gm);
-
- let gf=document.getElementById("flexRadioDefault2").value;
- console.log(gf);
- document.getElementById("gender").innerHTML=gf;
- document.write(gf);
-
- let pad=document.getElementById("inputAddress2").value;
- console.log(pad);
- document.getElementById("PermanentAddress").innerHTML=pad;
- document.write(pad);
-
- let UN=document.getElementById("uN").value;
- console.log(UN);
- document.getElementById("university").innerHTML=UN;
- document.write(UN);
  
- let col=document.getElementById("COL").value;
- console.log(col);
- document.getElementById("college").innerHTML=col;
- document.write(col);
+ let dob=document.getElementById("dob1").value;
+ document.getElementById("dob2").innerHTML=dob;
+ 
 
+ let ad=document.getElementById("Address").value;
+ document.getElementById("address").innerHTML=ad;
+ 
+ let padd=document.getElementById("Address2").value;
+ document.getElementById("PermanentAddress").innerHTML=padd;
+
+ let c=document.getElementById('c1').value;
+ document.getElementById('city2').innerHTML=c;
+ 
+
+
+ let pi=document.getElementById("Pin1").value;
+ 
+ document.getElementById("pin2").innerHTML=pi;
+ 
+
+ let un=document.getElementById('uN').value;
+ 
+
+ document.getElementById("university").innerHTML=un;
+
+
+
+ let cl=document.getElementById("COL1").value;
+ 
+ document.getElementById("College").innerHTML=cl;
+ 
 
  let cn=document.getElementById("CN").value;
- console.log(cn);
+ 
  document.getElementById("CourseName").innerHTML=cn;
- document.write(cn);
 
- let sem1=document.getElementById("sem").value;
- console.log(sem1);
- document.getElementById("semester").innerHTML=sem1;
- document.write(sem1);
 
- let padd=document.getElementById("inputAddress2").value;
- console.log(padd);
- document.getElementById("PermanentAddress").innerHTML=padd;
- document.write(padd);
-
+ let s=document.getElementById("sem").value;
+ 
+ document.getElementById("semester").innerHTML=s;
+ 
+ 
  let yop=document.getElementById("year").value;
- console.log(yop);
+ 
  document.getElementById("yearofpassing").innerHTML=yop;
- document.write(yop);
-
- let cgpa=document.getElementById("per").value;
- console.log(cgpa);
- document.getElementById("cgpa").innerHTML=cgpa;
- document.write(cgpa);
-
- let resume=document.getElementById("formFile").value;
- console.log(resume);
- document.getElementById("resume").innerHTML=resume;
- document.write(resume);
+ 
 
 
- let pt=document.getElementById("formFile1").value;
- console.log(pt);
- document.getElementById("photo").innerHTML=pt;
- document.write(pt);
+ let mrks=document.getElementById("per").value;
+ 
+ document.getElementById("cg2pa").innerHTML=mrks;
+ 
 
- let adno=document.getElementById("inputPassword4").value;
- console.log(adno);
- document.getElementById("aadhaar").innerHTML=adno;
- document.write(adno);
+ let sem1=document.getElementById("formFile1").value;
+ 
+ document.getElementById("resume2").innerHTML=sem1;
+ 
 
- let pan=document.getElementById("inputPassword8").value;
- console.log(pan);
- document.getElementById("PANID").innerHTML=pan;
- document.write(pan);
-
- let panpt=document.getElementById("formFile3").value;
- console.log(panpt);
- document.getElementById("PanCardPhoto").innerHTML=panpt;
- document.write(panpt);
-}
 
